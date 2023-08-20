990a65c388876c1a1710385c6e24019aa5992ef7
bernardjbs <bernardjbs@yahoo.com>
2023-08-20 15:16:13 +0800

    Added fields to currency pairs

--- database/migrations/2023_08_20_070345_create_currency_pairs_table.php
+++ database/migrations/2023_08_20_070345_create_currency_pairs_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('currency_pairs', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description');
             $table->timestamps();
         });
     }
